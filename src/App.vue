b8175529c9a32c9e81c4081803046014568a3ca5
yuyangw <39216513@qq.com>
2023-10-07 18:43:51 +0800

    css reset

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
+  <div :class="ns.b()">
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" :class="ns.e('logo')" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img
+        src="./assets/vue.svg"
+        :class="[ns.e('logo'), ns.em('logo', 'vue')]"
+        alt="Vue logo"
+      />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { useNamespace } from '@/hooks'
+
+defineProps<{ msg: string }>()
+
+const ns = useNamespace('main')
+</script>
+
+<style lang="scss">
+@use '@/assets/scss/mixins/bem.scss' as *;
+
+@include b(main) {
+  display: flex;
+  width: 960px;
+  margin: 0 auto;
+  padding-top: 40px;
+  justify-content: center;
+  gap: 40px;
+
+  @include e(logo) {
+    height: 120px;
+    will-change: filter;
+    transition: filter 300ms;
+
+    &:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+
+    @include m(vue) {
+      &:hover {
+        filter: drop-shadow(0 0 2em #42b883aa);
+      }
+    }
+  }
 }
 </style>
